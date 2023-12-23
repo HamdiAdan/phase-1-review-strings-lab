3010be21e68441e046ce2e9f18ec9e00caf406d4
Hamdi Adan <hhhaji013@gmail.com>
2023-12-23 12:50:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser ="Hamdi";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+//To uppercase//
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
